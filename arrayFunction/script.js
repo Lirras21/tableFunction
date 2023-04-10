e00418804dafaf58749156c799c1ff11e6ce9e5b
Nazar <nazarvelicko@gmail.com>
2023-04-10 18:31:02 +0300

    create object and array

---
+++ arrayFunction/script.js
@@ -0,0 +1,80 @@
+const usersData = {
+    id: 1,
+    firstName: '',
+    lastName: '',
+    age: 0,
+    gender: ''
+  };
+  const users = [
+      {
+          id: 1,
+          firstName: 'Nazar',
+          lastName: 'Velichko',
+          age: 21,
+          gender: 'Male'
+      },
+      {
+          id: 2,
+          firstName: 'Mykola',
+          lastName: 'Staryi',
+          age: 25,
+          gender: 'Male'
+      },
+      {
+          id: 3,
+          firstName: 'Petro',
+          lastName: 'Shchur',
+          age: 28,
+          gender: 'Male'
+      },
+      {
+          id: 4,
+          firstName: 'Angelina',
+          lastName: 'Moroz',
+          age: 45,
+          gender: 'Female'
+      },
+      {
+          id: 5,
+          firstName: 'Viktoria',
+          lastName: 'Danko',
+          age: 27,
+          gender: 'Female'
+      },
+      {
+          id: 6,
+          firstName: 'Oleh',
+          lastName: 'Chornenkyi',
+          age: 20,
+          gender: 'Male'
+      },
+      {
+          id: 7,
+          firstName: 'Yura',
+          lastName: 'Velichko',
+          age: 19,
+          gender: 'Male'
+      },
+      {
+          id: 8,
+          firstName: 'Dmytro',
+          lastName: 'Popko',
+          age: 22,
+          gender: 'Male'
+      },
+      {
+          id: 9,
+          firstName: 'Anton',
+          lastName: 'Velichko',
+          age: 17,
+          gender: 'Male'
+      },
+      {
+          id: 10,
+          firstName: 'Alina',
+          lastName: 'Shuba',
+          age: 17,
+          gender: 'Female'
+      }
+  ];
+  
